ab340af6d743845e64670aafe0c0d32308e86c19
Helge Stenström <h.stenstrom@gmail.com>
2019-11-21 21:51:21 +0100

    Update AuctionSniperEndToEndTest.java
    Fixing typos, make package-private

--- src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -8,27 +8,27 @@ import org.junit.jupiter.api.Test;
  * @author Helge Stenström on 2018-03-22.
  * @project AuctionSniper
  */
-public class AuctionSniperEndToEndTest {
+class AuctionSniperEndToEndTest {
     private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
     private final ApplicationRunner application = new ApplicationRunner();
 
     @Test
-    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
-        auction.startSellingItem(); // Step 1
-        application.startBiddingIn(auction); // Step 2
-        auction.hasReceivedJoinRequestFromSniper(); // Step 3
-        auction.announceClosed(); // Step 4
-        application.showsSniperHasLostAuction(); // Step 5
+    void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();                  // Step 1
+        application.startBiddingIn(auction);         // Step 2
+        auction.hasReceivedJoinRequestFromSniper();  // Step 3
+        auction.announceClosed();                    // Step 4
+        application.showsSniperHasLostAuction();     // Step 5
     }
 
     // Additional cleanup
     @AfterEach
-    public void stopAuction() {
+    void stopAuction() {
         auction.stop();
     }
 
     @AfterEach
-    public void stopApplication() {
-        auction.stop();
+    void stopApplication() {
+        application.stop();
     }
 }
